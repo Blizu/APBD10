357065746331191cf47ed3e7f6b935ffe2775c76
Piotr Bliżewski <piotr.blizewski@gmail.com>
2020-07-05 14:50:51 +0200

    done zad10

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using APBD10.Models;
+
+namespace APBD10.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            var db = new localDBContext();
+
+            return Ok(db.Student.ToList());
+        }
+
+        [HttpPut("{indexNumber}")]
+        public IActionResult UpdateStudent(string indexNumber, Student studentDto)
+        {
+            var db = new localDBContext();
+
+            var student = db.Student
+                .Where(s => s.IndexNumber == indexNumber)
+                .Single();
+
+            student.FirstName = studentDto.FirstName;
+            student.LastName = studentDto.LastName;
+            db.SaveChanges();
+
+            return Ok(student);
+        }
+
+        [HttpDelete("{indexNumber}")]
+        public IActionResult RemoveStudent(string indexNumber)
+        {
+            var db = new localDBContext();
+
+            var student = db.Student
+                .Where(s => s.IndexNumber == indexNumber)
+                .Single();
+
+            db.Remove(student);
+            db.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ Models/Enrollment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APBD10.Models
+{
+    public partial class Enrollment
+    {
+
+        public int IdEnrollment { get; set; }
+        public int Semester { get; set; }
+        public int IdStudy { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public Enrollment()
+        {
+            Student = new HashSet<Student>();
+        }
+
+        public virtual Studies IdStudyNavigation { get; set; }
+        public virtual ICollection<Student> Student { get; set; }
+    }
+}
